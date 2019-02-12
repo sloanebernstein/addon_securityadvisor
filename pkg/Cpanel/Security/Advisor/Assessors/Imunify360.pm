0c8f4c6543bbb6e534789480aff804b22265786b
Rikus Goodell <rikus.goodell@cpanel.net>
2019-02-12 11:25:07 -0600

    LC-10308: Move utility functions out into separate module.
    Everything related to checking the state of Imunify360 has been
    moved out into a separate module, Whostmgr::Imunify360, that is not
    part of the Security Advisor.

--- pkg/Cpanel/Security/Advisor/Assessors/Imunify360.pm
+++ pkg/Cpanel/Security/Advisor/Assessors/Imunify360.pm
@@ -31,27 +31,12 @@ use warnings;
 use base 'Cpanel::Security::Advisor::Assessors';
 
 use Cpanel::Config::Sources ();
-use Cpanel::cPStore         ();
-use Cpanel::DIp::MainIP     ();
-use Cpanel::Exception       ();
-use Cpanel::HTTP::Client    ();
-use Cpanel::JSON            ();
-use Cpanel::NAT             ();
-use Cpanel::Server::Type    ();
 use Cpanel::Version         ();
-use Cpanel::RPM             ();
+use Whostmgr::Imunify360    ();
 
 use Cpanel::Imports;
 
 our $IMUNIFY360_MINIMUM_CPWHM_VERSION = '11.79';    # we want it usable on both development and release builds for 11.80
-
-# These two ids were verified as correct 2019-02-11 using https://verify.cpanel.net/api/addons?ip=...... for a licensed IP address
-our $IMUNIFY360_PRODUCT_ID    = 'Imunify360';
-our $IMUNIFY360_PACKAGE_ID_RE = qr/\bIMUNIFY360\b/;    # e.g., PARTNERNAME-IMUNIFY360-UNLIMITED or ANOTHERNAME-IMUNIFY360-SOLO
-
-# These two short names were verified as correct 2019-02-08 using https://aa.store.manage.testing.cpanel.net/
-our $IMUNIFY360_STORE_SHORTNAME_UNL  = 'monthly_imunify360_unlimited';
-our $IMUNIFY360_STORE_SHORTNAME_SOLO = 'monthly_imunify360_solo';
 
 sub version {
     return '1.00';
@@ -77,8 +62,8 @@ sub generate_advice {
 sub _suggest_imunify360 {
     my ($self) = @_;
 
-    if ( !$self->_is_imunify360_licensed ) {
-        my $imunify360_price = _get_imunify360_price();
+    if ( !Whostmgr::Imunify360::is_imunify360_licensed() ) {
+        my $imunify360_price = Whostmgr::Imunify360::get_imunify360_price();
         my $store_url        = Cpanel::Config::Sources::get_source('STORE_SERVER_URL');
         my $url              = "$store_url/view/imunify360/license-options";
 
@@ -94,7 +79,7 @@ sub _suggest_imunify360 {
             block_notify => 1,                                                                                                                                                                                                                                                       # Do not send a notification about this
         );
     }
-    elsif ( !$self->_is_imunify360_installed ) {
+    elsif ( !Whostmgr::Imunify360::is_imunify360_installed() ) {
 
         $self->add_warn_advice(
             key          => 'Imunify360_install',
@@ -112,66 +97,6 @@ sub _suggest_imunify360 {
     }
 
     return 1;
-}
-
-sub _is_imunify360_licensed {
-    my ($self) = @_;
-
-    my $mainip = Cpanel::NAT::get_public_ip( Cpanel::DIp::MainIP::getmainserverip() );
-
-    my $http = Cpanel::HTTP::Client->new( timeout => 20 )->die_on_http_error();
-    my $response = $http->get( Cpanel::Config::Sources::get_source('VERIFY_URL') . q{/ipaddrs.cgi?ip=} . $mainip );
-    die Cpanel::Exception::create('HTTP') if !$response->{success};
-
-    my $data = Cpanel::JSON::Load( $response->{content} );
-
-    foreach my $license ( @{ $data->{current} } ) {
-        return 1
-          if $license->{product} eq $IMUNIFY360_PRODUCT_ID
-          && $license->{package} =~ $IMUNIFY360_PACKAGE_ID_RE
-          && $license->{status} eq 1    # Means 'active'
-          && $license->{valid} eq 1;
-    }
-
-    # For QA purposes:
-    #  This touch file doesn't grant any actual ability to use Imunify360 as if it were licensed; it just tricks
-    #  the Security Advisor into skipping the promotional message and going right to the "installed" check.
-    return 1 if -f '/var/cpanel/imunify360_skip_promo';
-
-    return 0;
-}
-
-sub _is_imunify360_installed {
-    my ($self) = @_;
-    my $rpm = Cpanel::RPM->new();
-
-    return $rpm->has_rpm(q{imunify360-firewall});
-}
-
-sub _get_imunify360_product {
-    my $store        = Cpanel::cPStore->new();
-    my $product_list = $store->get('products/cpstore');
-
-    my $is_cpanel_solo = ( Cpanel::Server::Type::get_max_users() == 1 );
-
-    my $product_shortname =
-        $is_cpanel_solo
-      ? $IMUNIFY360_STORE_SHORTNAME_SOLO
-      : $IMUNIFY360_STORE_SHORTNAME_UNL;
-
-    foreach my $product (@$product_list) {
-        return $product if $product->{'short_name'} eq $product_shortname;
-    }
-
-    return;
-}
-
-sub _get_imunify360_price {
-    my $product = _get_imunify360_product;
-    if ( ref $product eq 'HASH' && $product->{price} ) {
-        return $product->{price};
-    }
-    return;
 }
 
 1;
